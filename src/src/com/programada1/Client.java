8d23c3e1971a7661f5abc135044c6d137e818390
Jose Pablo Ramirez Mendez <jose.ramirez16@ucr.ac.cr>
2018-05-05 14:49:54 -0600

    Simulation classes added.

---
+++ src/src/com/programada1/Client.java
@@ -0,0 +1,4 @@
+package com.programada1;
+
+public class Client {
+}
---
+++ src/src/com/programada1/Sim.java
@@ -0,0 +1,8 @@
+package com.programada1;
+
+public class Sim {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
